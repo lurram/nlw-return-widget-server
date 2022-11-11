7615ace193b067b73266480ff37a4ef8149688f7
lurram <lurramsantos.00.ls@gmail.com>
2022-11-10 22:04:32 -0300

    Add Server

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,23 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer';
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "ec07cfea38d0ac",
+      pass: "a67e64059f71a3"
+    }
+  });
+
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail ({subject, body}: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Lurram Santos <lurramsantos.ap@hotmail.com>',
+            subject,
+            html: body,
+          })
+    }
+}
